791cea68bf0eb9619f9b3767fb8671b7207427be
Innokentii <jacksafj@gmail.com>
2022-03-03 19:23:11 +0300

    swipe Refresh

--- app/src/main/java/com/example/bf_kotlin_client/utils/GlobalFunctions.kt
+++ app/src/main/java/com/example/bf_kotlin_client/utils/GlobalFunctions.kt
@@ -1,16 +1,37 @@
 package com.example.bf_kotlin_client.utils
 
 import android.graphics.Bitmap
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-
-
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 
 class GlobalFunctions {}
-    @BindingAdapter("android:image_bitmap")
-    fun setImageBitmap(imageView: ImageView, bitmap: Bitmap
-    ) {
-        imageView.setImageBitmap(bitmap)
+
+@BindingAdapter("android:image_bitmap")
+fun setImageBitmap(
+    imageView: ImageView, bitmap: Bitmap,
+) {
+    imageView.setImageBitmap(bitmap)
+}
+@BindingAdapter("android:isRefresh")
+fun setRefresh(
+    swipeRefreshLayout: SwipeRefreshLayout, value:Boolean,
+) {
+    swipeRefreshLayout.isRefreshing=value
+}
+@BindingAdapter("android:onRefresh")
+fun setOnRefreshListener(
+    swipeRefreshLayout: SwipeRefreshLayout,
+    oldValue: SwipeRefreshLayout.OnRefreshListener?,
+    newValue: SwipeRefreshLayout.OnRefreshListener?,
+) {
+    if (oldValue != null) {
+        swipeRefreshLayout.setOnRefreshListener(oldValue)
     }
-var appConext:Int=0
+    if (newValue != null) {
+        swipeRefreshLayout.setOnRefreshListener(newValue)
+    }
+
+}
