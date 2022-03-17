4888aa4e769e4c25d002f63e742b72bb2dc288ed
GODofCAT <dis2827a@gmail.com>
2022-03-17 20:01:18 +0300

    done but i need B(N)IGGER images

--- app/src/main/java/com/example/bf_kotlin_client/adapters/products/RvAdapterProductsCategories.kt
+++ app/src/main/java/com/example/bf_kotlin_client/adapters/products/RvAdapterProductsCategories.kt
@@ -3,6 +3,7 @@ package com.example.bf_kotlin_client.adapters.products
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.databinding.ObservableField
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bf_kotlin_client.R
@@ -12,47 +13,66 @@ import com.example.bf_kotlin_client.databinding.ProductCategoryPreviewBinding
 import com.example.bf_kotlin_client.dtos.entities.ProductCategory
 import com.example.bf_kotlin_client.utils.AppFragmentManager.FragmentsName.ProductsInCategoryFragment
 import com.example.bf_kotlin_client.utils.GlobalVariables
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class RvAdapterProductsCategories(private var categories: ArrayList<ProductCategory>) :
     RecyclerView.Adapter<RvAdapterProductsCategories.ViewHolder>() {
-    private var globalVariables = GlobalVariables.instance
-    private var layoutInflater = LayoutInflater.from(globalVariables.applicationContext)
-    private var imageApiWorker = ImageApiWorker()
-    private var fragmentManager = globalVariables.fragmentManager
 
     inner class ViewHolder internal constructor(var binding: ProductCategoryPreviewBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        private var globalVariables = GlobalVariables.instance
+        private var imageApiWorker = ImageApiWorker()
+        var fieldTitle: ObservableField<String> = ObservableField("")
+            private set
         var fieldImage: ObservableField<Bitmap> = ObservableField(
-            imageApiWorker.getBitmapFromDrawableId(R.drawable.ic_launcher_background)
+            globalVariables.applicationContext.getDrawable(R.drawable.ic_launcher_background)
+                ?.toBitmap()
         )
             private set
+
         var productCategory = ProductCategory()
             set(value) {
                 field = value
+                fieldTitle.set(value.name)
+
                 GlobalScope.launch(Dispatchers.IO) {
                     var bitmap =
                         imageApiWorker.getPictureByName("productsCategories", value.pictureName)
                     fieldImage.set(bitmap)
                 }
             }
+
         fun openProductList() {
-            fragmentManager.openFragmentAboveMain(ProductsInCategoryFragment)
-            var binding =
-                fragmentManager.getCurrentFragmentBinding<FragmentProductsInCategoryBinding>()
+            var fm = globalVariables.fragmentManager
+            fm.openFragmentAboveMain(ProductsInCategoryFragment)
+            var binding = fm.getCurrentFragmentBinding<FragmentProductsInCategoryBinding>()
             var viewModel = binding!!.viewModel
+
             viewModel!!.category = productCategory
         }
     }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var binding = ProductCategoryPreviewBinding.inflate(layoutInflater)
+        var binding = ProductCategoryPreviewBinding.bind(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.product_category_preview, parent, false)
+        )
         return ViewHolder(binding)
+
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.viewModel = holder
+
         holder.productCategory = categories[position]
+
     }
+
     override fun getItemCount(): Int {
         return categories.size
     }
+
 }
