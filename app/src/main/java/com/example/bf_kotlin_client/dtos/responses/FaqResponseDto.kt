87b0b41f4c497c8b9504f4a8b2d51f4f2a0a0a9b
Innokentii <jacksafj@gmail.com>
2022-03-20 18:27:32 +0300

    faqs get all

---
+++ app/src/main/java/com/example/bf_kotlin_client/dtos/responses/FaqResponseDto.kt
@@ -0,0 +1,6 @@
+package com.example.bf_kotlin_client.dtos.responses
+
+import com.example.bf_kotlin_client.dtos.entities.Faq
+import java.util.ArrayList
+
+class FaqResponseDto(var faqs: ArrayList<Faq>)
--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/support/SupportMainPageViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/support/SupportMainPageViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.bf_kotlin_client.viewmodels.support
 
+import com.example.bf_kotlin_client.apiworkers.FaqApiWorker
+import com.example.bf_kotlin_client.dtos.entities.Faq
+import com.example.bf_kotlin_client.dtos.responses.FaqResponseDto
+import com.google.gson.Gson
+import java.util.*
+import kotlin.collections.ArrayList
+
 class SupportMainPageViewModel {
+    var faqApiWorker= FaqApiWorker()
+    var faqs:ArrayList<Faq> =arrayListOf()
+    init{
+        faqApiWorker.getAll {
+            var response = Gson().fromJson(it, FaqResponseDto::class.java)
+            faqs=response.faqs
+        }
+    }
 }
