7c1c4a066bd1ca7210648daa7d1307765201528f
Innokentii <jacksafj@gmail.com>
2022-04-05 19:35:39 +0300

    farmer fragment

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/farmers/FarmerViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/farmers/FarmerViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.bf_kotlin_client.viewmodels.farmers
 
+import android.graphics.Bitmap
+import com.example.bf_kotlin_client.R
+import com.example.bf_kotlin_client.apiworkers.ImageApiWorker
 import com.example.bf_kotlin_client.dtos.entities.Farmer
 
 class FarmerViewModel {
+    private var imageApiWorker = ImageApiWorker()
+
     var farmer: Farmer=Farmer()
+        set(value) {
+            field = value
+            picture = imageApiWorker.getPictureByName("farmers", value.photoPath)
+        }
+
+    var picture: Bitmap =
+        imageApiWorker.getBitmapFromDrawableId(R.drawable.ic_launcher_background)
+        private set
 }
