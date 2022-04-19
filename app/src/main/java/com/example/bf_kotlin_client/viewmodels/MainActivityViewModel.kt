bdf9e57354e503a027682f1aad532c958d0e462b
GODofCAT <dis2827a@gmail.com>
2022-04-19 17:55:37 +0300

    Fragments done

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
@@ -13,9 +13,11 @@ class MainActivityViewModel {
 
     private var menuItemIdToTabName: MutableMap<Int, AppFragmentManager.FragmentsName> =
         mutableMapOf(
-            R.id.profileSection to ProfileFragment,
-            R.id.createSection to CreateOfferFragment,
-            R.id.myRequestsSection to OffersFragment
+            R.id.productsSection to ProductsCategoriesFragment,
+            R.id.farmersSection to FarmersListFragment,
+            R.id.favouritesSection to FavoriteProductsFragment,
+            R.id.profileSection to ProfileAuthFragment,
+            R.id.supportSection to SupportQuestionsPageFragment
         )
 
     var onBackPressedCallback = object : OnBackPressedCallback(true) {
