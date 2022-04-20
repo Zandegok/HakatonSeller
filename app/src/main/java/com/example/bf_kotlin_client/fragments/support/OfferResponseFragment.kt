6962c289a4ba5decf0c0ce631818cf3ae56a5714
GODofCAT <dis2827a@gmail.com>
2022-04-20 18:19:26 +0300

    sdelal chto smog

---
+++ app/src/main/java/com/example/bf_kotlin_client/fragments/support/OfferResponseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.bf_kotlin_client.fragments.support
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bf_kotlin_client.databinding.FragmentOfferResponseBinding
+import com.example.bf_kotlin_client.viewmodels.OfferResponsesViewModel
+
+class OfferResponseFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentOfferResponseBinding.inflate(layoutInflater)
+
+        binding.viewModel = OfferResponsesViewModel()
+
+        return binding.root
+    }
+}
