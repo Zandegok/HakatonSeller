d465d734d5fe25ba3ac4a38493ad34ce5afc2cc6
Innokentii <jacksafj@gmail.com>
2022-04-20 15:35:35 +0300

    bottom navigation view hiding

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
@@ -1,16 +1,25 @@
 package com.example.bf_kotlin_client.viewmodels
 
+import android.opengl.Visibility
 import android.view.MenuItem
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.ObservableField
 import com.example.bf_kotlin_client.R
 import com.example.bf_kotlin_client.utils.AppFragmentManager
 import com.example.bf_kotlin_client.utils.AppFragmentManager.FragmentsName.*
 import com.example.bf_kotlin_client.utils.GlobalVariables
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivityViewModel {
     private var fragmentManager = GlobalVariables.instance.fragmentManager
-
+    var bottomNavigationViewEnabled = false
+        set(value) {
+            field = value
+            bottomNavigationViewVisibility.set(if (value) VISIBLE else INVISIBLE)
+        }
+    var bottomNavigationViewVisibility = ObservableField(INVISIBLE)
     private var menuItemIdToTabName: MutableMap<Int, AppFragmentManager.FragmentsName> =
         mutableMapOf(
             R.id.profileSection to ProfileFragment,
@@ -25,6 +34,9 @@ class MainActivityViewModel {
     }
 
     fun onItemSelected(menuItem: MenuItem): Boolean {
+        if (!bottomNavigationViewEnabled) {
+            return false
+        }
         var tabName = menuItemIdToTabName[menuItem.itemId]
         fragmentManager.showTab(tabName!!)
         return true
