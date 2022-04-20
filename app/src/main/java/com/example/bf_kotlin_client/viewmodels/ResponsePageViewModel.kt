c3692b0a52768c18e050497e20fe707683b8929d
GODofCAT <dis2827a@gmail.com>
2022-04-21 00:24:12 +0300

    version naeb

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/ResponsePageViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/ResponsePageViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.bf_kotlin_client.viewmodels
 
+import androidx.databinding.ObservableField
 import com.example.bf_kotlin_client.apiworkers.AuthApiWorker
 import com.example.bf_kotlin_client.dtos.entities.Buyer
 import com.example.bf_kotlin_client.dtos.entities.Response
@@ -8,6 +9,10 @@ import com.google.gson.Gson
 
 class ResponsePageViewModel {
     var authApiWorker = AuthApiWorker()
+    var phone = ObservableField("")
+    var price = ObservableField("")
+    var name = ObservableField("")
+    var comment = ObservableField("")
     var seller = Buyer()
     var response = Response()
         set(value) {
@@ -16,6 +21,12 @@ class ResponsePageViewModel {
                 var sellersResponse = Gson().fromJson(it, SellersResponse::class.java)
                 seller = sellersResponse.seller.first { it.id == value.sellerId}
             }
+            name.set(seller.name)
+            phone.set(seller.phoneNumber)
+            comment.set(response.comment)
+            price.set(response.price.toString())
         }
+
+
 
 }
