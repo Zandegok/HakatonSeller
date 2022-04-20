313646b3db505c6d8d0250451385bb5556bf0eb3
Innokentii <jacksafj@gmail.com>
2022-04-20 20:16:31 +0300

    responses view

--- app/src/main/java/com/example/bf_kotlin_client/dtos/entities/Response.kt
+++ app/src/main/java/com/example/bf_kotlin_client/dtos/entities/Response.kt
@@ -4,3 +4,6 @@ class Response(
     var id: Int = 0, var price: Int = 0, var comment: String="",
     var requestId: Int = 0, var sellerId: Int = 0,
 )
+{
+    fun getPriceString(): String {return price.toString()}
+}
