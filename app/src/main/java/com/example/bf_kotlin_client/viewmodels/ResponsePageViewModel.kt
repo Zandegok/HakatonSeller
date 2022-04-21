6b1ce253b9cb01cfe5aebca8ee6388f1b562dea1
Innokentii <jacksafj@gmail.com>
2022-04-21 10:37:29 +0300

    work

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/ResponsePageViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/ResponsePageViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.bf_kotlin_client.viewmodels
 
+
 import androidx.databinding.ObservableField
 import com.example.bf_kotlin_client.apiworkers.AuthApiWorker
 import com.example.bf_kotlin_client.dtos.entities.Buyer
@@ -9,24 +10,21 @@ import com.google.gson.Gson
 
 class ResponsePageViewModel {
     var authApiWorker = AuthApiWorker()
-    var phone = ObservableField("")
-    var price = ObservableField("")
-    var name = ObservableField("")
-    var comment = ObservableField("")
     var seller = Buyer()
+        set(value) {
+            field = value
+            sellerName.set(seller.name)
+            sellerPhone.set(seller.phoneNumber)
+        }
+    var sellerName = ObservableField("")
+    var sellerPhone = ObservableField("")
     var response = Response()
         set(value) {
             field = value
             authApiWorker.getAllSellers {
                 var sellersResponse = Gson().fromJson(it, SellersResponse::class.java)
-                seller = sellersResponse.seller.first { it.id == value.sellerId}
+                seller = sellersResponse.seller.first { it.id == value.sellerId }
             }
-            name.set(seller.name)
-            phone.set(seller.phoneNumber)
-            comment.set(response.comment)
-            price.set(response.price.toString())
         }
-
-
 
 }
